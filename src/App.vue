d67ef7424962c0af3ab2309c2a0765230a1f2bd8
Eshan Dilmina <106302589+eshanceylonsoft@users.noreply.github.com>
2023-04-28 18:20:02 +0530

    option API

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup>
+<!-- Composition API -->
+
+<!-- <script setup>
    import {ref} from "vue";
    const count =ref(0);
 
@@ -8,14 +10,34 @@
    const substractFromCount=()=>{
     count=count-1;
    }
+</script> -->
+
+
+<!-- Option API  -->
+<script>
+export default ({
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    addToCount() {
+      this.count = this.count + 1;
+    },
+    substractFromCount() {
+      this.count = this.count - 1;
+    }
+  }
+})
 </script>
 <template>
   <main>
     <div>
       <h4>The current count is...</h4>
       <h1>{{ count }}</h1>
-      <button @click="addToCount()">-</button>
-      <button @click="substractFromCount()">+</button>
+      <button @click="addToCount()">+</button>
+      <button @click="substractFromCount()">-</button>
     </div>
   </main>
 </template>
@@ -31,10 +53,12 @@ main {
   justify-content: center;
 
 }
+
 button {
-    margin: 1vw;
-  }
-div{
+  margin: 1vw;
+}
+
+div {
   text-align: center;
   font-family: 'Courier New', Courier, monospace;
 }
